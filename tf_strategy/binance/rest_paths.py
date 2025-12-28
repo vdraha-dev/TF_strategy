06ceae276822d1bfcc6efc96839a139499873363
d-laslo <ice_bear@ukr.net>
2025-12-28 15:27:18 +0200

    renamed root directory

--- tf_strategy/binance/rest_paths.py
+++ tf_strategy/binance/rest_paths.py
@@ -1,12 +1,13 @@
-from pydantic import BaseModel, computed_field, Field, model_validator
+from pydantic import BaseModel, Field, computed_field, model_validator
 
 __all__ = ["rest_path"]
+
 
 class Path(BaseModel):
     version: str = Field(default="v3")
     public: PublicSubPath | None = None
     private: PrivateSubPath | None = None
-    
+
     @model_validator(mode="after")
     def build_subpaths(self):
         self.public = PublicSubPath(version=self.version)
@@ -19,12 +20,12 @@ rest_path = Path()
 
 class PublicSubPath(BaseModel):
     version: str
-    
+
     @computed_field
     @property
     def klines(self) -> str:
         return f"/api/{self.version}/klines"
-    
+
 
 class PrivateSubPath(BaseModel):
     version: str
