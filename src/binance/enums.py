581d26b2057409089598ad5b32c3eeeef1042e30
d-laslo <ice_bear@ukr.net>
2025-12-28 13:35:37 +0200

    added timeframe enum

---
+++ src/binance/enums.py
@@ -0,0 +1,19 @@
+from enum import StrEnum
+
+
+class TimeInterval(StrEnum):
+    _1s = "1s"
+    _1m = "1m"
+    _3m = "3m"
+    _5m = "5m"
+    _15m = "15m"
+    _30m = "30m"
+    _1h = "1h"
+    _2h = "2h"
+    _4h = "4h"
+    _6h = "6h"
+    _8h = "8h"
+    _12h = "12h"
+    _1d = "1d"
+    _2d = "3d"
+    _1m = "1m"
