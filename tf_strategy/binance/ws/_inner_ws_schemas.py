22f5c835f31e1f356d2cfac1994453bc897dc378
d-laslo <ice_bear@ukr.net>
2025-12-30 22:19:37 +0200

    updated inner schema

--- tf_strategy/binance/ws/_inner_ws_schemas.py
+++ tf_strategy/binance/ws/_inner_ws_schemas.py
@@ -1,7 +1,9 @@
 from collections import namedtuple
-from dataclasses import dataclass
+from collections.abc import Iterable
+from itertools import count
 
-from tf_strategy.common.async_event import AsyncHandler
+import orjson
+
 from tf_strategy.common.enums import TimeInterval
 
 from ..schemas import Symbol
@@ -11,15 +13,61 @@ KlineKey = namedtuple(
 )
 
 
-@dataclass(frozen=True)
-class HandlerToken:
-    subscription_key: KlineKey
-    handler: AsyncHandler
-
-
 class WSKeyCreator:
     """Class for creating uniquely interpreted keys for each WS subscription"""
 
     @classmethod
-    def kline_key(cls, symbol: Symbol, time_interval: TimeInterval) -> KlineKey:
-        return KlineKey(symbol=symbol.symbol, time_interval=time_interval)
+    def kline_key(
+        cls, symbol: Symbol | str, time_interval: TimeInterval | str
+    ) -> KlineKey:
+        if not isinstance(symbol, str):
+            symbol = symbol.symbol
+
+        if not isinstance(time_interval, str):
+            time_interval = time_interval.value
+
+        return KlineKey(symbol=symbol.lower(), time_interval=time_interval)
+
+
+class PublicSubscriptionCreator:
+    """"""
+
+    _id = count(1)
+    _subscribe_core = {"method": "SUBSCRIBE"}
+    _unsubscribe_core = {"method": "UNSUBSCRIBE"}
+
+    @classmethod
+    def _new_core(cls, core: dict, params: Iterable[str]):
+        return {
+            **core,
+            "id": next(cls._id),
+            "params": list(params),
+        }
+
+    @classmethod
+    def _format_symbol(cls, symbol: Symbol | str):
+        if isinstance(symbol, str):
+            return symbol.lower()
+        return symbol.symbol.lower()
+
+    @classmethod
+    def kline_subscription_msg(
+        cls, symbol: Symbol | str, time_interval: TimeInterval | str
+    ):
+        return orjson.dumps(
+            cls._new_core(
+                cls._subscribe_core,
+                (f"{cls._format_symbol(symbol)}@kline_{time_interval}",),
+            )
+        ).decode()
+
+    @classmethod
+    def kline_unsubscription_msg(
+        cls, symbol: Symbol | str, time_interval: TimeInterval | str
+    ):
+        return orjson.dumps(
+            cls._new_core(
+                cls._unsubscribe_core,
+                (f"{cls._format_symbol(symbol)}@kline_{time_interval}",),
+            )
+        ).decode()
