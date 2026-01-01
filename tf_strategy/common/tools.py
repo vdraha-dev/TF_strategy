d61177a6ae45dae376638b97fc59d195c6bb5404
d-laslo <ice_bear@ukr.net>
2026-01-01 13:36:07 +0200

    sign tools added

--- tf_strategy/common/tools.py
+++ tf_strategy/common/tools.py
@@ -1,5 +1,13 @@
 import asyncio
+import base64
 from contextlib import asynccontextmanager
+from urllib.parse import urlencode
+
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric.types import (
+    PrivateKeyTypes,
+    PublicKeyTypes,
+)
 
 
 @asynccontextmanager
@@ -15,8 +23,29 @@ async def set_event(event: asyncio.Event):
 @asynccontextmanager
 async def remove_event(event: asyncio.Event):
     """Temporarily removes the event"""
-    event.set()
+    event.clear()
     try:
         yield
     finally:
-        event.clear()
+        event.set()
+
+
+def load_private_key_from_pep(
+    path: str, password: bytes | None = None
+) -> PrivateKeyTypes:
+    """Load private key from .pem file."""
+    with open(path, "rb") as f:
+        return serialization.load_pem_private_key(f.read(), password=password)
+
+
+def load_public_key_from_pep(path: str) -> PublicKeyTypes:
+    """Load public key from .pem file."""
+    with open(path, "rb") as f:
+        return serialization.load_pem_public_key(f.read())
+
+
+def sign_msg(private_key: PrivateKeyTypes, payload: dict):
+    """Format, encode and sign the payload msg using private_key."""
+    return base64.b64encode(
+        private_key.sign(urlencode(payload).encode("utf-8"))
+    ).decode()
