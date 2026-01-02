136f2fb4cacc04216c4601d8ac9e8aab1bc5e204
d-laslo <ice_bear@ukr.net>
2026-01-02 15:24:46 +0200

    specialized schemas for binance added

--- tf_strategy/binance/schemas.py
+++ tf_strategy/binance/schemas.py
@@ -1,14 +1,28 @@
 from decimal import Decimal
 
-from tf_strategy.common.schemas import Kline as CommonKline
-from tf_strategy.common.schemas import Symbol as CommonSymbol
+from tf_strategy.common.schemas import (
+    Kline as CommonKline,
+)
+from tf_strategy.common.schemas import (
+    Order as CommonOrder,
+)
+from tf_strategy.common.schemas import (
+    OrderReport as CommonOrderReport,
+)
+from tf_strategy.common.schemas import (
+    Symbol as CommonSymbol,
+)
 
 
 class Symbol(CommonSymbol):
+    """Binance Symbol"""
+
     _fmt = "{}{}"
 
 
 class Kline(CommonKline):
+    """Binance Kline"""
+
     @classmethod
     def from_list(cls, raw: list) -> Kline:
         return cls(
@@ -40,3 +54,39 @@ class Kline(CommonKline):
             taker_buy_base_volume=Decimal(raw["V"]),
             taker_buy_quote_volume=Decimal(raw["Q"]),
         )
+
+
+class Order(CommonOrder):
+    """Binance Order"""
+
+    def create_order_payload(self) -> str:
+        payload = {
+            "symbol": self.symbol.symbol,
+            "side": self.side.value,
+            "type": self.type.value,
+        }
+
+        if self.time_in_force:
+            payload["timeInForce"] = self.time_in_force.value
+
+        if self.quantity:
+            payload["quantity"] = str(self.quantity)
+
+        if self.quote_order_qty:
+            payload["quoteOrderQty"] = str(self.quote_order_qty)
+
+        if self.price:
+            payload["price"] = str(self.price)
+
+        if self.new_client_order_id:
+            payload["newClientOrderId"] = self.new_client_order_id
+
+        if self.stop_price:
+            payload["stopPrice"] = self.stop_price
+
+
+class OrderReport(CommonOrderReport):
+    """Binance OrderReport"""
+
+    @classmethod
+    def from_dict(cls, raw: dict) -> OrderReport: ...
